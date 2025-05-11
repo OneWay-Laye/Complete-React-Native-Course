8217ddbcd123d35b4b2652539da3d536c5dcda99
Alayé Taylor <54966515+OneWay-Laye@users.noreply.github.com>
2025-05-11 01:58:26 -0400

    This commit creates Themed Button and adds them to login and register pages

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,12 +1,19 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Pressable, Text } from 'react-native'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
 
 //Themed Components
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 
 const Login = () => {
+
+  const handleSubmit = () => {
+    console.log('login form submited')
+  }
+
   return (
     <ThemedView style={styles.container}>
         
@@ -14,6 +21,10 @@ const Login = () => {
         <ThemedText title={true} style={styles.title}>
             Login To Your Account
         </ThemedText>
+
+       <ThemedButton onPress={handleSubmit}>
+          <Text style={{ color: '#f2f2f2' }}>Login</Text>
+       </ThemedButton>
 
         <Spacer height={100} />
         <Link href='/register'>
@@ -29,10 +40,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
+        alignItems: 'center'
       },
       title: {
         textAlign: "center",
         fontSize: 18,
         marginBottom: 30
+      },
+      btn: {
+        backgroundColor: Colors.primary,
+        padding: 15,
+        borderRadius: 5,
+      },
+      pressed: {
+        opacity: 0.8
       }
 })
