2a61819e1dbd4dd3f40677d75646b4f53cec378e
Alayé Taylor <54966515+OneWay-Laye@users.noreply.github.com>
2025-05-10 23:24:12 -0400

    This commit shows how to access User Light/Dark mode
    Since we are able to access this setting we can now modify the page to follow that setting

--- app/about.jsx
+++ app/about.jsx
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, useColorScheme } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 
+import { Colors } from '../constants/Colors'
+
 const About = () => {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor: theme.background}]}>
       <Text style={styles.title}>About Page</Text>
       <Link style={styles.link} href="/">Home</Link>
       <Link style={styles.link} href="/contact">Contact</Link>
